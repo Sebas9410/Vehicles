732efebf51b4db0d396c128cb3df512f11ca6ef9
Sebas9410 <53840547+Sebas9410@users.noreply.github.com>
2021-10-12 17:12:19 -0500

    Add vehicles type

---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Tipo de vehiculo")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener mas de 50 carácteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]   
+        public string Description { get; set; }
+
+
+    }
+}
